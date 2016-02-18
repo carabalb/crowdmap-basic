cb47e9681ff6d085b81fe7cb67a588869e698fd2
carabalb <lennycaraballo@gmail.com>
2016-02-18 12:36:50 -0500

    Update insert_function.sql

--- insert_function.sql
+++ insert_function.sql
@@ -11,7 +11,7 @@ AS $$
 DECLARE 
     _the_geom GEOMETRY;
 	--The name of your table in cartoDB
-	_the_table TEXT := 'crowdmap_basic';
+	meaningful_table_name TEXT := 'crowdmap_basic';
 BEGIN
     --Convert the GeoJSON to a geometry type for insertion. 
     _the_geom := ST_SetSRID(ST_GeomFromGeoJSON(_geojson),4326); 
